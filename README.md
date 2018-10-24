243681804175b8269cce34a939f69076b0ee4371
Fausto Guillen and Kaveh Saleminejad <kaveh@>
2018-10-24 11:49:31 -0700

    all done

---
+++ README.md
@@ -0,0 +1,36 @@
+# _Pit Latin_
+
+#### _single Webpage for Epicodus, 10.24.2018_
+
+#### By _**Fausto Guillen and Kaveh Saleminejad**_
+
+## Description
+
+_For words beginning with a vowel, add "way" to the end.
+
+For words beginning with one or more consonants, move all of the first consecutive consonants to the end, and add "ay".
+
+If the first consonants include "qu", move the "u" along with the "q". Don't forget about words like "squeal" where "qu" doesn't come first!
+
+For words beginning with "y", treat "y" as a consonant._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open a simple HTTP server in the top level of the cloned directory. For a mac, run the following command in your terminal:_   
+* _python -m SimpleHTTPServer_
+* _Then open your web browser of choice to localhost:8000_
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _Javascript_
+* _jQuery_
+* _Bootstrap_
+
+### License
+
+*This software is licensed under the MIT license.*
+
+Copyright (c) 2018 **_Fausto Guillen and Kaveh Saleminejad_**
---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  var vowels = ["o","e","a","u","i","O","E","A","U","I"];
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "X", "Z", "W", "Y"];
+  $("#formOne").submit(function(event) {
+  event.preventDefault();
+    var str = $("#sentence").val ();
+    var words = str.split (' ');
+    var chars = [];
+    var resultWord='';
+    var finalResult="";
+    words.forEach(function(word){
+      chars = word.split ('');
+      var newWord= "";
+      var num=0;
+      vowels.forEach(function(vowel){
+        if (chars[0]==vowel) {
+          newWord=word+"way";
+          finalResult+=newWord+" ";
+        };
+      });
+      chars.forEach(function(char,index){
+        if (ifConsonants(char,consonants) && index==num) {
+          num++
+        };
+        if (index=1) {
+          if (char=="u" && chars[0]=="q") {
+            num++;
+          };
+        };
+      });
+      if (num>0) {
+        newWord = word.substr(num) + word.substr(0, num);
+        newWord+="ay";
+        finalResult+=newWord+" ";
+      };
+    });
+    $("#story").show();
+    $("#story").append ("<p>"+finalResult+"</p>");
+  });
+});
+function ifConsonants(str,consonants){
+  for(var i=0;i<consonants.length;i++){
+    if (str==consonants[i]) {
+      return true;
+      };
+    };
+  return false;
+  };
